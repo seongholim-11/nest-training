b16efc1f172961742cf39137068b9343cb19aa93
seongholim <damadu01@gmail.com>
2026-03-15 21:13:41 +0900

    첫커밋

---
+++ src/step1.ts
@@ -0,0 +1,22 @@
+/**
+ * [실습 과제: TypeScript 기본기]
+ * 
+ * 1. User 인터페이스 정의 (id, username, email, isActive, createdAt)
+ * 2. users 배열 (모킹 데이터) 생성
+ * 3. getActiveUsernames 함수 작성
+ * 4. 결과 출력
+ */
+
+// 1. User 인터페이스를 정의하세요.
+export interface User {
+  
+}
+
+// 2. 가짜(Mock) 사용자 데이터를 생성하세요.
+// const users: User[] = [ ... ];
+
+// 3. getActiveUsernames 함수를 구현하세요.
+
+
+// 4. 실행 결과를 확인하세요.
+// console.log(getActiveUsernames(users));
